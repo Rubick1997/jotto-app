2278fc832e9547fe4d5d602d542dfc43183b7a27
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-06-24 15:18:56 -0700

    guessed words

--- src/GuessedWords.test.tsx
+++ src/GuessedWords.test.tsx
@@ -13,7 +13,7 @@ const setup = (props = {}) => {
 };
 
 describe("if there are no words guesses", () => {
-  let wrapper:ShallowWrapper;
+  let wrapper: ShallowWrapper;
   beforeEach(() => {
     wrapper = setup({ guessedWords: [] });
   });
@@ -23,9 +23,35 @@ describe("if there are no words guesses", () => {
   });
 
   test("renders instructions to guesss a word", () => {
-    const instructions = findByTestAttr(wrapper,"guess-instructions");
-    expect(instructions.text().length).not.toBe(0)
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(instructions.text().length).not.toBe(0);
   });
 });
 
-describe("if there are words guesses", () => {});
+describe("if there are words guesses", () => {
+  let wrapper: ShallowWrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "car", letterMatchCount: 1 },
+    { guessedWord: "plane", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders 'guessed words' section", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test("correct number of guessed words", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordsNode.length).toBe(guessedWords.length);
+  });
+});
