a2fd22d92d098d103a4ae80405a71859bf061e89
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-06-24 18:14:32 -0700

    input changing tests

--- src/Input.test.tsx
+++ src/Input.test.tsx
@@ -12,3 +12,17 @@ test("renders without error", () => {
   const inputComponent = findByTestAttr(wrapper, "component-input");
   expect(inputComponent.length).toBe(1);
 });
+
+describe("state controlled input field", () => {
+  test("state updates with value of input box upon change", () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    const mockEvent = {target:{value:'train'}}
+    inputBox.simulate("change",mockEvent)
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train')
+  });
+});
--- src/Input.tsx
+++ src/Input.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
 
-function Input() {
-    return (
-        <div data-test="component-input">
-            
-        </div>
-    )
+function Input(props: { secretWord?: string }) {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  return (
+    <div data-test="component-input">
+      <form action="">
+        <TextField
+          label="type a word"
+          data-test="input-box"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <Button data-test="submit-button" variant="contained" color="primary">
+          Primary
+        </Button>
+      </form>
+    </div>
+  );
 }
 
-export default Input 
+export default Input;
