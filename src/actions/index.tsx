b5b9efd0d98bf3eab1f651c28325fba0fe9d5467
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-06-28 09:11:56 -0700

    CORRECT_GUESS action creator and guess

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,8 +1,15 @@
 import axios from "axios";
 
-export const getSecretWord =() => {
+export const actionTypes = {
+  CORRECT_GUESS: "CORRECT_GUESS",
+};
+
+export function correctGuess() {
+  return { type: actionTypes.CORRECT_GUESS };
+}
+
+export const getSecretWord = () => {
   //return response from server
   //write actuall action in Redux/contet sections
-  return axios.get("http://localhost:3030")
-  .then(response => response.data);
+  return axios.get("http://localhost:3030").then((response) => response.data);
 };
